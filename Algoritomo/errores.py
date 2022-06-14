0c3e0c07593362924b4dbba368f39dabe08bcf0e
crmardones94 <c.mardones.p94@gmail.com>
2022-06-14 17:27:27 -0400

    ok

--- Algoritomo/errores.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('cls')
-try:
-    b=[1,2,3]
-    print(b[4])
-except Exception as ex:
-    print(f"error: {ex}")
--- Algoritomo/numbpy.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-lista = np.array([1,2,3,4,5,6])
-
-print(lista.ndim)
