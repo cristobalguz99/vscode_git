a3025932d0412671997b550b6bdcc11744298e61
crmardones94 <c.mardones.p94@gmail.com>
2022-06-14 18:15:10 -0400

    funciona de pana

---
+++ ejercicio2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import metodos as me
+import os  
+
+
+os.system('cls')
+cont = 0
+impares = 0
+
+arreglo_1 = np.random.randint(0, 1001, size=10)
+
+for i in arreglo_1:
+    if i % 2 == 0:
+        cont = cont + 1
+    else:
+        impares = impares + i
+        
+    valida = me.es_primo(int(i))
+    if valida == True:
+        print(f'El numero {i} es primo \n')
+
+
+print("Arreglo: ", arreglo_1)
+print("Cantidad de elementos pares: ", cont)
+print("Suma de elementos impares: ", impares)
+
--- metodos.py
+++ metodos.py
@@ -5,3 +5,9 @@ def multi(a,b):
 
 def introduction(first_name, last_name):
     print("Hola, mi nombre es", first_name, last_name)
+
+def es_primo(num):
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+    return True
