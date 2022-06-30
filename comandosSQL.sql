ed0142ac5e6a91068678f696447b8f13d118fdd1
crmardones94 <c.mardones.p94@gmail.com>
2022-06-30 17:31:51 -0400

    se agrega insert

--- comandosSQL.sql
+++ comandosSQL.sql
@@ -115,7 +115,7 @@ CREATE TABLE sindicatos (
     nombre_sindicato VARCHAR2(250) NOT NULL,
     cuota_inicial    NUMBER NOT NULL,
     cuota_mensual    NUMBER NOT NULL,
-    "_Persona"       NUMBER NOT NULL,
+    personas_id       NUMBER NOT NULL,
     empresa_id       NUMBER NOT NULL
 );
 CREATE TABLE telefonos (
@@ -277,7 +277,7 @@ ALTER TABLE sindicatos
         REFERENCES empresa ( id_empresa );
 
 ALTER TABLE sindicatos
-    ADD CONSTRAINT sindicatos_personas_fk FOREIGN KEY ( "_Persona" )
+    ADD CONSTRAINT sindicatos_personas_fk FOREIGN KEY ( personas_id )
         REFERENCES personas ( id_persona );
 
 ALTER TABLE telefonos
@@ -291,3 +291,104 @@ ALTER TABLE telefonos
 ALTER TABLE vehiculos
     ADD CONSTRAINT vehiculos_tipo_vehiculo_fk FOREIGN KEY ( tipo_vehiculo_id )
         REFERENCES tipo_vehiculo ( id_tipo_vehiculo );
+
+INSERT INTO estado_civil (id_estado_civil, estado_civil) VALUES (1, "Casado/a")
+INSERT INTO estado_civil (id_estado_civil, estado_civil) VALUES (2, "Soltero/a")
+INSERT INTO estado_civil (id_estado_civil, estado_civil) VALUES (3, "Viudo/a")
+INSERT INTO estado_civil (id_estado_civil, estado_civil) VALUES (4, "Divorciado/a")
+INSERT INTO estado_civil (id_estado_civil, estado_civil) VALUES (5, "Separado/a")
+
+INSERT INTO regiones (id_region,nombre_region) VALUES (1,"Tarapacá")
+INSERT INTO regiones (id_region,nombre_region) VALUES (2,"Antofagasta")
+INSERT INTO regiones (id_region,nombre_region) VALUES (3,"Atacama")
+INSERT INTO regiones (id_region,nombre_region) VALUES (4,"Coquimbo")
+INSERT INTO regiones (id_region,nombre_region) VALUES (5,"Valparaíso")
+INSERT INTO regiones (id_region,nombre_region) VALUES (6,"O'Higgins")
+INSERT INTO regiones (id_region,nombre_region) VALUES (7,"Maule")
+INSERT INTO regiones (id_region,nombre_region) VALUES (8,"BioBio")
+INSERT INTO regiones (id_region,nombre_region) VALUES (9,"Araucania")
+INSERT INTO regiones (id_region,nombre_region) VALUES (10,"Los Lagos")
+INSERT INTO regiones (id_region,nombre_region) VALUES (11,"Aysen")
+INSERT INTO regiones (id_region,nombre_region) VALUES (12,"Magallanes y antartica Chilena")
+INSERT INTO regiones (id_region,nombre_region) VALUES (13,"Metropolitana")
+INSERT INTO regiones (id_region,nombre_region) VALUES (14,"los Rios")
+INSERT INTO regiones (id_region,nombre_region) VALUES (15,"Arica y Parinacota")
+INSERT INTO regiones (id_region,nombre_region) VALUES (16,"Nuble")
+
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (1,'Arica',15)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (2,'Parinacota',15)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (3,'Iquique',1)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (4,'Tamarugal',1)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (5,'Antofagasta',2)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (6,'El Loa',2)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (7,'Tocopilla',2)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (8,'Copiapó',3)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (9,'Chañaral',3)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (10,'Huasco',3)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (11,'Elqui',4)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (12,'Choapa',4)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (13,'Limari',4)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (14,'Valparaíso',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (15,'Marga Marga',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (16,'Isla de Pascua',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (17,'Los Andes',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (18,'Petorca',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (19,'Quillota',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (20,'San Antonio',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (21,'San Felipe',5)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (22,'Cachapoal',6)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (23,'Cardenal Caro',6)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (24,'Colchagua',6)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (25,'Talca',7)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (26,'Cauquenes',7)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (27,'Curico',7)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (28,'Linares',7)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (29,'Concepción',8)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (30,'Arauco',8)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (31,'Bío- Bío',8)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (32,'Diguillín',16)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (33,'Itata',16)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (34,'Punilla',16)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (35,'Cautín',9)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (36,'Malleco',9)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (37,'Valdivia',14)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (38,'Ranco',14)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (39,'Llanquihue',10)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (40,'Chiloe',10)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (41,'Osorno',10)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (42,'Palena',10)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (43,'Coihaique',11)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (44,'Aisén',11)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (45,'Capitan Prat',11)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (46,'General Carrera',11)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (47,'Magallanes',12)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (48,'Antártica Chilena',12)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (49,'Tierra del Fuego',12)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (50,'Ultima Esperanza',12)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (51,'Santiago',13)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (52,'Cordillera',13)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (53,'Chacabuco',13)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (54,'Maipo',13)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (55,'Melipilla',13)
+INSERT INTO provincias (id_provincia, nombre_provincia, regiones_id) VALUES (56,'Talagante',13)
+
+INSERT INTO comunas (id_comuna, nombre_comuna, provincias_id) VALUES (1, 'Peñalolen',51)
+INSERT INTO comunas (id_comuna, nombre_comuna, provincias_id) VALUES (2, 'Peralillo',24)
+INSERT INTO comunas (id_comuna, nombre_comuna, provincias_id) VALUES (3, 'Penco',29)
+INSERT INTO comunas (id_comuna, nombre_comuna, provincias_id) VALUES (4, 'Perquero',35)
+INSERT INTO comunas (id_comuna, nombre_comuna, provincias_id) VALUES (5, 'Peñaflor',56)
+
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (1,'Chileno')
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (2,'Peruano')
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (3,'Boliviano')
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (4,'Argentino')
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (5,'Brazileño')
+INSERT INTO nacionalidades (id_nacionalidades, nacionalidad) VALUES (6,'Venezolano')
+
+INSERT INTO generos(id_genero, tipo_genero) VALUES (1, 'Masculino')
+INSERT INTO generos(id_genero, tipo_genero) VALUES (2, 'Femenino')
+INSERT INTO generos(id_genero, tipo_genero) VALUES (3, 'Otro')
+INSERT INTO generos(id_genero, tipo_genero) VALUES (4, 'No me identifico')
+INSERT INTO generos(id_genero, tipo_genero) VALUES (5, 'Helicoptero Apache')
+
+INSERT INTO personas VALUES (1, '','18829876','1','Cristian', '','Mardones','xxxxx','22-05-2022','cr.mardones@duocuc.cl',00000000, 123,1,1,2,1)
