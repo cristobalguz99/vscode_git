a67bce5086c28ec7ccfc95a19b802fb3be076857
crmardones94 <c.mardones.p94@gmail.com>
2022-10-25 09:47:20 -0300

    se cambio algo

--- Dia 4/dia 4.py
+++
@@ -1,8 +0,0 @@
-valores = [1, 2, 3, 4, 5, 6, 9.5]
-valores_pares=[n for n in valores if n%2==0] 
-print(valores_pares)
-
-
-temperatura_fahrenheit = [32, 212, 275]
-grados_fahrenheit=[(n-32)*(5/9) for n in temperatura_fahrenheit]
-print(grados_fahrenheit)
